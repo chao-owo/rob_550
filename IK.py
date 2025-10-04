6fc32621ad999f6993b4af3b74c54091af55894c
chao-owo <wenwang1212@gmail.com>
2025-10-04 09:57:46 -0400

    Edit: actual parameter

--- IK.py
+++ IK.py
@@ -1,30 +1,26 @@
 import numpy as np
 from math import atan2, acos, sqrt, sin, cos, pi
 
-def analytical_IK(x, y, z, pitch=pi / 2, row = pi/2,elbow_up=True):
-    l1 = 200
+def analytical_IK(x, y, z, roll = pi/2,elbow_up=True):
+    l1 = 205.73
     l2 = 200
-    l3 = 50
+    l3 = 151
 
-    d1 = 100  # Height of joint 1
-    d2 = 50  # Offset from joint 1 to joint 2
-    base_height = d1 + d2
+    base_height = 104
 
     theta1 = atan2(y, x) * 180 / pi
     theta3 = acos((x ** 2 + y ** 2 - l1 ** 2 - l2 ** 2) / 2 * l1 * l2) * 180 / pi
 
     r = sqrt(x ** 2 + y ** 2)
 
-    r_wrist = r - l3 * cos(pitch)
-    z_wrist = z - base_height - l3 * sin(pitch)
+    r_wrist = r - l3 * cos(roll)
+    z_wrist = z - base_height - l3 * sin(roll)
 
     # Distance from joint 2 to wrist
     d_to_wrist = sqrt(r_wrist ** 2 + z_wrist ** 2)
 
     print(f"Distance to wrist: {d_to_wrist:.2f}")
-    print(f"Link lengths: l1={l1}, l2={l2}")
 
-    # Check if target is reachable
     if d_to_wrist > (l1 + l2):
         print(f"too far! Distance={d_to_wrist:.2f}, Max reach={l1 + l2}")
         return None
@@ -37,7 +33,9 @@ def analytical_IK(x, y, z, pitch=pi / 2, row = pi/2,elbow_up=True):
         theta3 = -theta3
 
     theta2 = (atan2(y, x)-atan2(l2*sin(theta3),l1+l2*cos(theta3)))* 180 / pi
-    theta4 = (pitch - theta2 - theta3)* 180 / pi
+    offset_angle =acos((200**2+ 205.73**2- 50**2)/(2*200*205.73))*180/pi
+    theta2 = theta2-offset_angle
+    theta4 = (roll - theta2 - theta3)* 180 / pi
 
     result = [theta1, theta2, theta3, theta4]
     return result
@@ -46,13 +44,13 @@ if __name__ == "__main__":
     target_x = 200
     target_y = 200
     target_z = 300
-    target_pitch = pi / 2
+    target_roll = pi / 2
 
     print("\nElbow-UP solution:")
-    solution_up = analytical_IK(target_x, target_y, target_z, pitch=target_pitch, elbow_up=True)
+    solution_up = analytical_IK(target_x, target_y, target_z, roll=target_roll, elbow_up=True)
     print(solution_up)
 
     print("\nElbow-DOWN solution:")
-    solution_down = analytical_IK(target_x, target_y, target_z, pitch=target_pitch, elbow_up=False)
+    solution_down = analytical_IK(target_x, target_y, target_z, roll=target_roll, elbow_up=False)
     print(solution_down)
 
